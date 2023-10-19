0727055b5d6b169b6a51a05ffa99606b5a6d177d
Florian Löw <florian.loew@edu.tbz.ch>
2023-10-19 15:43:24 +0200

    Added Responsive page

--- pfadimh/src/app/Anschlag/OFI/page.tsx
+++ pfadimh/src/app/Anschlag/OFI/page.tsx
@@ -1,110 +1,106 @@
 "use client";
-import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+} from "@nextui-org/react";
 
 export default function MyCard() {
   return (
-    <div className="mx-28 bg-white">
-      <div className="mx-14 bg-white">
-        <div className="flex justify-center items-start bg-white space-x-16">
-          <Card className="w-15 h-13 mt-5 bg-red-100">
-            <CardHeader>
-              <h2 className="text-3xl font-bold text-red-600">Über uns.</h2>
-            </CardHeader>
-            <CardBody>
-              <div
-                className="text-black font-sans"
-                style={{ whiteSpace: "pre-line" }}
-              >
-                Wir sind die Piostufe. In unserer Stufe werden die
-                Teilnehmer*innen selbstständig. Es werden spannende Aktivitäten
-                geplant und Anlässe
-                {"\n"}
-                {"\n"}
-                durchgeführt. Auch findet ein Austausch mit verschiedenen Pios
-                aus
-                {"\n"}
-                der Umgebung statt. Die Piostufe ist eine erlebnisreiche Zeit,
-                in
-                {"\n"}
-                {"\n"}
-                der die Teilnehmer*innen die wichtigste Grundlage lernen, um
-                gute
-                {"\n"}
-                Leiter*innen zu sein und mit gleichaltrigen tolle Erlebnisse zu
-                sammeln.
-              </div>
-            </CardBody>
-          </Card>
-          <Card className="h-13 w-13 bg-red-100">
-            <CardBody className="random_img">
-              <img
-                src="https://image.jimcdn.com/app/cms/image/transf/dimension=399x10000:format=png/path/s4c5d6f225e690de2/image/i1750ff59e7550964/version/1667412256/image.png"
-                alt="Ich bin voll Däbii"
-              />
-            </CardBody>
-          </Card>
-        </div>
+    <div className="p-10 flex justify-center">
+      <div className="grid grid-cols-1 md:w-[768px] sm:grid-cols-3 gap-8">
+        <Card className="w-full sm:col-span-2 bg-red-100 ">
+          <CardHeader>
+            <h3 className="text-3xl font-bold text-red-600">
+              Unsere nächste Aktivität.
+            </h3>
+          </CardHeader>
+          <CardBody>
+            <div className="text-black" style={{ whiteSpace: "pre-line" }}>
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua.{"\n"}
+                At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+                dolor sit amet. Lorem ipsum dolor sit amet,{"\n"}
+                consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                invidunt ut labore et dolore magna aliquyam erat, sed diam
+                voluptua. At vero eos et accusam et justo duo dolores et ea
+                rebum. Stet clita kasd gubergren,{"\n"}
+                no sea takimata sanctus est Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+          </CardBody>
+        </Card>
 
-        <div className="flex justify-center items-start bg-white space-x-16 mt-5">
-          <Card className="w-15 h-13 mt-5 bg-red-100">
-            <CardHeader>
-              <h3 className="text-3xl font-bold text-red-600">Kalender</h3>
-            </CardHeader>
-            <CardBody>
-              <div className="text-black" style={{ whiteSpace: "pre-line" }}>
-                <h4>Planungstag</h4>
-                <p className="text-blue-600">22. Oktober 2023</p>
-                {"\n"}
-                <h4>Heimwoche</h4>
-                <p className="text-blue-600">
-                  22. Oktober 2023 - 28. Oktober 2023
-                </p>
-              </div>
-            </CardBody>
-          </Card>
-          <Card className="bg-red-100">
-            <CardHeader>
-              <h3 className="text-3xl font-bold text-red-600">
-                Unsere nächste Aktivität.
-              </h3>
-            </CardHeader>
-            <CardBody>
-              <div className="text-black" style={{ whiteSpace: "pre-line" }}>
-                <p>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  aliquyam erat, sed diam voluptua.{"\n"}
-                  At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-                  dolor sit amet. Lorem ipsum dolor sit amet,{"\n"}
-                  consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                  invidunt ut labore et dolore magna aliquyam erat, sed diam
-                  voluptua. At vero eos et accusam et justo duo dolores et ea
-                  rebum. Stet clita kasd gubergren,{"\n"}
-                  no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                </p>
-              </div>
-            </CardBody>
-          </Card>
-        </div>
-        <div className="mx-14">
-          <div className="flex mb-4">
-            <Card className="w-15 h-30 bg-red-100">
-              <CardHeader>
-                <h3 className="text-3xl font-bold text-red-600">
-                  Leiter*Innen
-                </h3>
-              </CardHeader>
-              <CardBody>
-                <img
-                  src="your_leaders_image_url_here"
-                  alt="leaders_description_here"
-                  className="text-black"
-                />
-              </CardBody>
-            </Card>
-          </div>
-        </div>
+        <Card className=" w-full bg-red-100 ">
+          <CardHeader>
+            <h3 className="text-3xl font-bold text-red-600">Kalender</h3>
+          </CardHeader>
+          <CardBody>
+            <div className="text-black" style={{ whiteSpace: "pre-line" }}>
+              <h4>Planungstag</h4>
+              <p className="text-blue-600">22. Oktober 2023</p>
+              {"\n"}
+              <h4>Heimwoche</h4>
+              <p className="text-blue-600">
+                22. Oktober 2023 - 28. Oktober 2023
+              </p>
+            </div>
+          </CardBody>
+        </Card>
+
+        <Card className="sm:col-span-2 w-full bg-red-100 p-5 ">
+          <CardHeader>
+            <h2 className="text-3xl font-bold text-red-600">Über uns.</h2>
+          </CardHeader>
+          <CardBody>
+            <div
+              className="text-black font-sans"
+              style={{ whiteSpace: "pre-line" }}
+            >
+              Wir sind die Piostufe. In unserer Stufe werden die
+              Teilnehmer*innen selbstständig. Es werden spannende Aktivitäten
+              geplant und Anlässe
+              {"\n"}
+              {"\n"}
+              durchgeführt. Auch findet ein Austausch mit verschiedenen Pios aus
+              {"\n"}
+              der Umgebung statt. Die Piostufe ist eine erlebnisreiche Zeit, in
+              {"\n"}
+              {"\n"}
+              der die Teilnehmer*innen die wichtigste Grundlage lernen, um gute
+              {"\n"}
+              Leiter*innen zu sein und mit gleichaltrigen tolle Erlebnisse zu
+              sammeln.
+            </div>
+          </CardBody>
+        </Card>
+
+        <Card className=" hidden sm:block  bg-red-100 ">
+          <CardBody className="random_img">
+            <Image
+              src="https://image.jimcdn.com/app/cms/image/transf/dimension=399x10000:format=png/path/s4c5d6f225e690de2/image/i1750ff59e7550964/version/1667412256/image.png"
+              alt="Ich bin voll Däbii"
+            />
+          </CardBody>
+        </Card>
+
+        <Card className="w-full sm:col-span-3 bg-red-100 ">
+          <CardHeader>
+            <h3 className="text-3xl font-bold text-red-600">Leiter*Innen</h3>
+          </CardHeader>
+          <CardBody>
+            <img
+              src="your_leaders_image_url_here"
+              alt="leaders_description_here"
+              className="text-black"
+            />
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
