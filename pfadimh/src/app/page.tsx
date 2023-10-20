ffc9cab1659ea985dc47967e8a3c5c8178906d8b
Cyrill-A-NY <cyrill.weber@noseryoung.com>
2023-10-20 10:55:02 +0200

    added parallex

--- pfadimh/src/app/page.tsx
+++ pfadimh/src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { Link } from "@nextui-org/link";
+
+import ParallaxMountains from "@/app/Components/ParalaxMountains";
 
 export default function Home() {
   return (
     <main>
+      <ParallaxMountains />
       <Link href="/Anschlag/OFI">
         <p> Page</p>
       </Link>
